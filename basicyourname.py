5a696beadd0d7ad4e8b418dfb9d2962d0452e7f5
Arya Teli <100777925+AryaTeli@users.noreply.github.com>
2022-09-26 22:42:51 +0530

    Create basicyourname.py

---
+++ basicyourname.py
@@ -0,0 +1,10 @@
+def print_full_name(first, last):
+    first = first_name
+    last = last_name
+    fullname = "Hello "+first+" "+last+"!"+" You just delved into python."
+    print(fullname)
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
